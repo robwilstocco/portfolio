070ea285b309e7cf1ab73d4d3fbf1e6e456192cf
Robert Wilson <robwilstocco@gmail.com>
2024-12-14 15:42:44 -0300

    Initial creation of projects section

--- src/sections/Projetos/Projetos.tsx
+++ src/sections/Projetos/Projetos.tsx
@@ -1,26 +1,46 @@
 import Container from "../../components/Container/Container";
 import styles from './Projetos.module.css';
 import { FaAngleLeft, FaAngleRight } from 'react-icons/fa';
+import projects from '../../data/projects.json';
+import { useState } from "react";
 
 const Projetos = () => {
+    const [index, setIndex] = useState(0);
+    const currentProject = projects[index];
+    const [animationClass, setAnimationClass] = useState("");
+
+    const handleNextProject = () => {
+        setAnimationClass("animation-scale");
+        setTimeout(() => {
+            setIndex(i => (i < projects.length - 1 ? i + 1 : 0));
+            setAnimationClass("");
+        }, 500);
+    };
+
+    const handleLastProject = () => {
+        setAnimationClass("animation-scale");
+        setTimeout(() => {
+            setIndex(i => (i > 0 ? i - 1 : projects.length - 1));
+            setAnimationClass("");
+        }, 500);
+    };
+
     return (
         <Container id='projetos'>
             <div className={`${styles.wrapper} animation-revealing-left`}>
                 <h2 className={styles.title}>{'<Projetos/>'}</h2>
                 <div className={styles.carousel}>
-                    {/* <button className={styles.leftButton}> */}
-                    <FaAngleLeft />
-                    {/* </button> */}
-                    <div className={styles.project}>
-                        <h3>Project name</h3>
-                        <img className={styles.aboutImage} src="https://place-hold.it/300x200" alt="project" />
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                            nisi ut aliquip ex ea commodo consequat.
-                        </p>
+                    <button className={styles.leftButton} onClick={handleLastProject}>
+                        <FaAngleLeft />
+                    </button>
+                    <div className={`${styles.project} ${animationClass}`}>
+                        <h3>{currentProject.name}</h3>
+                        <img className={styles.aboutImage} src={currentProject.image} alt="project" />
+                        <p>{currentProject.description}</p>
                     </div>
-                    <button><FaAngleRight /></button>
+                    <button className={styles.rightButton} onClick={handleNextProject}>
+                        <FaAngleRight />
+                    </button>
                 </div>
             </div>
         </Container>

eb245034a87c232f1c13f35a26fee9347d2519a0
Robert Wilson <robwilstocco@gmail.com>
2024-12-23 17:18:58 -0300

    Strict mode removed

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import './styles/reset.css';
 import './styles/global.css';
 import './styles/animations.css';
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Layout from './components/Layout/Layout';
 
@@ -10,7 +9,5 @@ const root = ReactDOM.createRoot(
 );
 
 root.render(
-  <React.StrictMode>
-    <Layout />
-  </React.StrictMode>
+  <Layout />
 );
